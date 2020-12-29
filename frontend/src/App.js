ed6c362241bf7aa2080f2cd00ccec8a40155a5a5
colie31 <colie4272@gmail.com>
2020-12-28 22:55:40 -0500

    Worked a bit on homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,9 @@ import SignupFormPage from "./components/SignupFormPage";
 // import LoginFormPage from "./components/LoginFormPage";
 import * as sessionActions from "./store/session";
 import Navigation from "./components/Navigation";
+//pics
+// import Logo from './pics/logos/logo1.png'
+import Logo2 from './pics/logos/logo2.jpg'
 
 function App() {
   const dispatch = useDispatch();
@@ -14,19 +17,26 @@ function App() {
   }, [dispatch]);
 
   return (
-    <>
-      <Navigation isLoaded={isLoaded} />
-      {isLoaded && (
-        <Switch>
-          {/* <Route path="/login" >
-            <LoginFormPage />
-          </Route> */}
-          <Route path="/signup">
-            <SignupFormPage />
-          </Route>
-        </Switch>
-      )}
-    </>
+    <div className='homeBox'>
+      <div className='logoBox'>
+        <img className='logo' src={Logo2} ></img>
+      </div>
+      <div>
+        <Navigation isLoaded={isLoaded} />
+      </div>
+      <div>
+        {isLoaded && (
+          <Switch>
+            {/* <Route path="/login" >
+              <LoginFormPage />
+            </Route> */}
+            <Route path="/signup">
+              <SignupFormPage />
+            </Route>
+          </Switch>
+        )}
+      </div>
+    </div>
   );
 }
 
