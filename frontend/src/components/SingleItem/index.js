16c5595de55a799af6d01c49634134ff928ca45e
colie31 <colie4272@gmail.com>
2020-12-31 11:57:37 -0500

    added cart reducer

--- frontend/src/components/SingleItem/index.js
+++ frontend/src/components/SingleItem/index.js
@@ -4,6 +4,10 @@ import { useDispatch, useSelector } from "react-redux";
 import { useEffect, useState } from "react";
 //import fetch
 import { fetch } from "../../store/csrf";
+import BeautyStars from "beauty-stars";
+//css
+import './SingleItem.css'
+
 
 //need to fetch inventory from database
 //dispatch fetch( w/ thunk function)
@@ -25,7 +29,6 @@ const SingleItem = () => {
         return anItem.id === itemId;
     })
   });
-  console.log(item)
 
   // const [item, setItem] = useState(null);
 
@@ -44,7 +47,31 @@ const SingleItem = () => {
 
   return (
     <>
-      {item && <h1>{item.name}</h1>}
+      {item && (
+        <div className="container">
+          <div>
+            <h1>{item.name}</h1>
+          </div>
+          <div className="imageContainer">
+            <img
+              src={item.url}
+              style={{ height: 500, width: 520, objectFit: "contain" }}
+            />
+          </div>
+          <BeautyStars
+            value={item.rating}
+            editable={false}
+            // onChange={}
+          />
+          <div>Amount Sold: {item.amountSold}</div>
+          <div>Price ${item.price}</div>
+          <div>Quantity: {item.quantity}</div>
+          <div className='descriptionBox'>
+            <p>{item.description}</p>
+          </div>
+          <div></div>
+        </div>
+      )}
     </>
   );
 };
--- frontend/src/store/cart.js
+++ frontend/src/store/cart.js
@@ -5,8 +5,17 @@
 //and if they can leave a comment or not
 
 //action
-
+const ADD_ITEM = 'addAnItem/CART';
+const REMOVE_ITEM = 'removeAnItem/CART';
+const UPDATE_COUNT = 'updateQuantityInCart/CART';
+const EMPTY = 'emptyCart/CART';
 //action creator
+export const addItem = (itemId) => {
+    return {
+        type: ADD_ITEM,
+        itemId
+    };
+};
 
 //thunks
 
@@ -16,6 +25,13 @@ const initialState = {}
 const CartReducer = (state = initialState, action) => {
     let newState;
     switch(action.type) {
+        case ADD_ITEM:
+            newState = Object.assign({}, state)
+            newState[action.itemId] = {
+                id: action.itemId,
+                count: 1,
+            }
+            return newState;
         default:
             return state;
     }
