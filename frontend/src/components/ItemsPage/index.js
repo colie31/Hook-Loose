e46d2dacbeac1b8dde7ecf62cf0979bdbe28c51b
colie31 <colie4272@gmail.com>
2021-04-21 16:15:43 -0400

    updated price view and item rendered

--- frontend/src/components/ItemsPage/index.js
+++ frontend/src/components/ItemsPage/index.js
@@ -26,32 +26,26 @@ const ItemsPage = () => {
 
 
         return (
-            <>
             <div className='itemsContainer storeContainer'>
-                {inventory.map(item => {
-                    return (
-                      <div key={item.id}>
-                        <div>
-                          <img
-                            alt=''
-                            className='images'
-                            src={item.url}
-                            style={{ height: 200, width: 220, objectFit: 'contain' }}
-                          />
-                        </div>
-                        <div>{item.name}</div>
-                        <div>{item.price}</div>
-                        <div>
-                        <Link to={`/items/${item.id}`}>
-                          <button>View Item</button>
-                        </Link>
-                          <button onClick={(e) => {dispatch(addItem(item))}}>Add To Cart</button>
-                        </div>
-                      </div>
-                    );
-                })}
+              {inventory.map(item => {
+                  return (
+                    <div className="item" key={item.id}>
+                      <img
+                        alt=''
+                        className='images'
+                        src={item.url}
+                        style={{ height: 200, width: 220, objectFit: 'contain' }}
+                      />
+                      <div>{item.name}</div>
+                      <div>{`$ ${item.price}`}</div>
+                      <Link to={`/items/${item.id}`}>
+                        <button>View Item</button>
+                      </Link>
+                        <button onClick={(e) => {dispatch(addItem(item))}}>Add To Cart</button>
+                    </div>
+                  );
+              })}
             </div>
-            </>
         );
     }
 
